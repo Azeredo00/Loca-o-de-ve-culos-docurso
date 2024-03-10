09941dbd563aa08b703a7c638b22ebb51beeeed6
Azeredo00 <161282466+Azeredo00@users.noreply.github.com>
2024-03-10 16:32:35 -0300

    Add files via upload

---
+++ tentativa de crud WEB 2/dashboard.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="all">
+        <div class="header">
+            <div>
+                <a 
+                    href="https://www.roblox.com/games/9865958871/BIG-Paintball-2#!/game-instances">
+                </a>
+                Ver Veículos
+            </div>
+            <div>
+                <a
+                    href="https://www.roblox.com/games/9865958871/BIG-Paintball-2#!/game-instances">
+                </a>
+                Minha conta
+            </div>
+        </div>
+        <div class="content">
+            <div class="vehicles">
+                <div class="imgdiv">
+                    <img src="Resources/Hilux.png" alt="" />            
+                </div>
+                <div class="rentinfo">
+                    <button class="rentbutton"> Marcar retirada
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ tentativa de crud WEB 2/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="all">
+        <div class="header">
+            <div>
+                <a 
+                    href="https://www.roblox.com/games/9865958871/BIG-Paintball-2#!/game-instances">
+                </a>
+                Ver Veículos
+            </div>
+            <div>
+                <a
+                    href="https://www.roblox.com/games/9865958871/BIG-Paintball-2#!/game-instances">
+                </a>
+                Minha conta
+            </div>
+        </div>
+        <div class="logincontent">
+            <form action="auth.php" method="post">
+                <label for="user_email">Email:
+                <input type="email" name="user_email" id="user_email" required>
+                </label>
+                <label for="user_password">Senha:
+                <input type="password" name="user_password" id="user_password" required minlength="8">
+                </label>
+                <button type="submit">Entrar
+                </button>
+            </form>            
+            <?php
+            session_start();
+            if (!empty($_SESSION['msg_login_error'])) :
+            ?>
+                <p><?= $_SESSION['msg_login_error'] ?></p>
+            <?php
+                unset($_SESSION['msg_login_error']);
+            endif;
+            ?>
+        </div>
+    </div>
+</body>
+</html>
